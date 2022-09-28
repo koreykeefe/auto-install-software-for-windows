689e69e8f4315c55f4a544001e46fdaa40ee6336
Korey Keefe <88954147+koreykeefe@users.noreply.github.com>
2022-09-28 01:22:59 -0400

    Update and rename batchInstall.ps1 to install_script.ps1

---
+++ install_script.ps1
@@ -0,0 +1,8 @@
+winget install Google.Chrome --silent
+winget install Spotify.Spotify --silent
+winget install Discord.Discord --silent
+winget install Valve.Steam --silent
+
+winget install Microsoft.Office --silent
+winget install Figma.Figma --silent
+winget install Oracle.VirtualBox --silent
